e9550430274b090e906275b0e3a9c9caa84e5af4
Jason Zhu <zhujohn111@gmail.com>
2022-05-22 12:30:52 -0400

    Solution 3

--- Project Euler Solutions/Problem 03 Largest prime factor.py
+++ Project Euler Solutions/Problem 03 Largest prime factor.py
@@ -4,8 +4,9 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 '''
 
-#A prime number (or prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself. 
-
+# A prime number (or prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself. 
+# I made a function to do the prime factorization and give the largest prime factor as well, but it is not efficient when the number is larger than a billion.
+# It will take a significant amount of time to solve this question. Will find a better way to solve this problem later.
 def largest_prime_factor(n):
     ls_of_primes=[]
     prime_factorization=''
