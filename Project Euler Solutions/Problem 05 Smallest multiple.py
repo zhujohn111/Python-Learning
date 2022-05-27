81b685583a617d9fc6966bff3a9fd7ebdf1cc49b
Jason Zhu <zhujohn111@gmail.com>
2022-05-27 02:11:43 -0400

    modified

--- Project Euler Solutions/Problem 05 Smallest multiple.py
+++ Project Euler Solutions/Problem 05 Smallest multiple.py
@@ -14,8 +14,8 @@ def smallest_num():
     x=20
     while True:
         if  x%11==0 and x%12==0 and x%13==0 and x%14==0 and x%15==0 and x%16==0 and x%17==0 and x%18==0 and x%19==0 and x%20==0:
-         print(x)
-         break
+            print(x)
+            break
         x+=1
 
 smallest_num()
